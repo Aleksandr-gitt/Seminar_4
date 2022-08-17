96adb8fb58996c6cef2f4773ecddef73aa2c91b1
Alex <sanyasan123@mail.ru>
2022-08-17 21:53:25 +0300

    H3

--- Home_3/Program.cs
+++ Home_3/Program.cs
@@ -6,27 +6,27 @@ int[] Array = new int[8];
 int length = Array.Length;
 Console.Write("[");
 
-void NewArray(int[] Array)
-{
-for(int i=0; i<length; i++)
-{
-    Array[i] = new Random().Next(1,100);
-    Console.Write($" {Array[i]},");
-}
-Console.Write(" ]");
-}
-
-NewArray(Array);
-
-
-// 2 Вариант (без запатой после последней цифры) вместо 10-20 строчек
-
-// int i = 0;
-// while(i<length-1)
+// void NewArray(int[] Array)
+// {
+// for(int i=0; i<length; i++)
 // {
 //     Array[i] = new Random().Next(1,100);
 //     Console.Write($" {Array[i]},");
-//     i++;
+// }
+// Console.Write(" ]");
 // }
 
-// Console.Write($" {Array[length-1]} ]");
+// NewArray(Array);
+
+
+// 2 Вариант (без запатой после последней цифры) вместо 9-19 строчек
+
+int i = 0;
+while(i<length-1)
+{
+    Array[i] = new Random().Next(1,100);
+    Console.Write($" {Array[i]},");
+    i++;
+}
+
+Console.Write($" {Array[length-1]} ]");
